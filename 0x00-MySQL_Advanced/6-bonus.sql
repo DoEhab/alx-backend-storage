0a4b20fed26905a0926bb57c89bf9bf649c78874
dohaehab <dohaehab@gmail.com>
2025-05-02 02:28:00 +0300

    task 1

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,16 +8,15 @@ CREATE PROCEDURE AddBonus(
 )
 BEGIN
     DECLARE project_id INT;
-
+	-- insert/update the new project 
     INSERT INTO projects (name)
     VALUES (in_project_name)
     ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id);
-
+	-- get the lastinserted proj ID
     SET project_id = LAST_INSERT_ID();
-
+	-- update the user score
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (in_user_id, project_id, in_score);
 END;
 //
-
 DELIMITER ;
