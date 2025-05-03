c1300ca4c957f13974d344ff8a70e60363e56cc4
dohaehab <dohaehab@gmail.com>
2025-05-04 00:15:39 +0300

    task 0

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,13 @@
--- change valid email upon insert
-DROP PROCEDURE IF EXISTS `ComputeAverageScoreForUser`
+-- calculate and update score for a user
+DROP PROCEDURE IF EXISTS `ComputeAverageScoreForUser`;
+
 DELIMITER |
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN in_user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN in_user_id INT)
 BEGIN
-DECLARE avg_score FLOAT;
-
-SELECT avg(score) INTO avg_score
-FROM corrections
-WHERE user_id = in_user_id;
-
-UPDATE users
-SET average_score = IFNULL(avg_score, 0)
-WHERE id = in_user_id;
-
+	UPDATE users
+	SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = in_user_id)
+	WHERE id = in_user_id;
 END;
 |
 
