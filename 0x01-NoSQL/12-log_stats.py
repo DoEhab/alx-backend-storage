3b97ebe4dfcefeee708338727b6db8aa9cbda5b7
dohaehab <dohaehab@gmail.com>
2025-05-10 01:24:23 +0300

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,21 +4,27 @@ script to print logs
 """
 from pymongo import MongoClient
 
-"""
-connecting to mongo DB
-count logs method and status
-"""
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.logs
-db_collection = db.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print(f"{db_collection.count_documents({})} logs")
+def print_stats():
+    """
+    connecting to mongo DB
+    count logs method and status
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    db_collection = db.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print("Methods:")
-for method in methods:
-    count = db_collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
+    print(f"{db_collection.count_documents({})} logs")
 
-status_check = db_collection.count_documents({"method": "GET", "path": "/status"})
-print(f"{status_check} status check")
+    print("Methods:")
+    for method in methods:
+        count = db_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = db_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == '__main__':
+    print_stats()
