a6d77c65a8d40c336852a6157fb1a4786d56c1f0
dohaehab <dohaehab@gmail.com>
2025-05-09 21:39:39 +0300

    task 0

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,5 +13,5 @@ def update_topics(mongo_collection, name, topics):
     """
     mongo_collection.update_many(
         {"name": name},
-        {"Sset": {"topics": topics}}
+        {"$set": {"topics": topics}}
     )
